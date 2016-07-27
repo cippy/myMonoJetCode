8ccb5320151bd9402b118bb35d2133d53498b285
Marco Cipriani <cippy91@gmail.com>
2016-07-27 11:16:31 +0200

    fixes

--- scripts/createDirectories.sh
+++ scripts/createDirectories.sh
@@ -6,28 +6,29 @@ wwwDir="/afs/cern.ch/user/m/mciprian/www/"
 basePath=$CMSSW_BASE/src
 
 # this macro should be invoked passing the name of config file, where directories'names are provided
-# the name passed can be the absolute path or simply it's name, or any relatve path, because we will get the <name>.txt part and build again the absolute path
+# the name passed can be the absolute path or simply it's name, or any relative path, because we will get the <name>.txt part and build again the absolute path
 # we are assuming the file is in myMonoJetCode/config directory, so please keep to this convention  
 
-if [ $# -eq 0 ]; 
+if [ $# -lt 2 ]; 
 then 
-    echo "ERROR: no argument passed to $0. Launch as --> prompt> source $0 <configFile>"
-    echo "Exiting and returning 5"
-    return 5
+    echo "ERROR: too few arguments passed to $0. Launch as --> prompt> source $0 <configFile> <last_folder_name>"
+    echo "Exiting"
+    return 1
 fi
 
 fileToGrep=$1
 absPathToConfig=${basePath}/myMonoJetCode/config/
-fileToGrep=${fileToGrep#*/}  # remove everything before <name>.txt (remove nothing if no '/' is present
+fileToGrep=${fileToGrep##*/}  # remove everything before <name>.txt (remove nothing if no '/' is present)
 #fileToGrep="monojet_signalRegion_config.txt"
-fileToGrep=${absPathToConfig=}${fileToGrep}
+fileToGrep=${absPathToConfig}${fileToGrep}
 
 #check existance of file
-if [ ! -f "$fileToGrep" ]; then
+if [ ! -f "$fileToGrep" ]; 
+then
     echo "ERROR: file $fileToGrep"
     echo "not found in $absPathToConfig !"
-    echo "Exiting and returning 5"
-    return 5
+    echo "Exiting"
+    return 1
 fi
 
 echo "changing directory to --> ${basePath}"
@@ -43,17 +44,19 @@ cd -
 #cd ${basePath}/myMonoJetCode/src
 
 dirTocreate=`cat ${fileToGrep} | grep DIRECTORY_PATH | awk '{print $3}'`  # name is 3rd object in line
-finalDir=`cat ${fileToGrep} | grep DIRECTORY_NAME | awk '{print $3}'`
+#finalDir=`cat ${fileToGrep} | grep DIRECTORY_NAME | awk '{print $3}'`
+finalDir=$2 # second argument is passed from main, because there might be some suffixes in the name given in config file
 finalDir=${dirTocreate}${finalDir}
 mkdir -p ${basePath}${finalDir}
 
 # ex: finalDir=/myMonoJetCode/output/monojet/80X/lumi_7p65fb/SignalRegion
 #echo "\$finalDir = $finalDir"
 #echo $finalDir
-#echo ${finalDir#/myMonoJetCode/output/}  # remove shortestpattern matching '/myMonoJetCode/output/' from the beginning
+#echo ${finalDir#/myMonoJetCode/output/}  # remove shortest pattern matching '/myMonoJetCode/output/' from the beginning
 dirToCreateInWWW=${finalDir#/myMonoJetCode/output/}
 echo "mkdir -p ${wwwDir}${dirToCreateInWWW}"
 mkdir -p ${wwwDir}${dirToCreateInWWW}
 cd ${wwwDir}
-./copyphp
+./copyphp.sh
 cd -
+
