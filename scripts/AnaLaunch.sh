f89fe073a4a1cb258b723ff193b6c47b6ebda9e1
Marco Cipriani <cippy91@gmail.com>
2016-07-27 11:16:56 +0200

    fixes

--- scripts/AnaLaunch.sh
+++ scripts/AnaLaunch.sh
@@ -5,35 +5,48 @@
 # bsub -q cmsan -J <jobname> -o <output-logfile> "source <script_name.sh>"
 #
 #
-MY_PATH=$CMSSW_BASE/src/myMonoJetCode
-MY_CFG_PATH=$MY_PATH/config
-MY_MACRO_PATH=$MY_PATH/macro
-cd $MY_PATH/src
+base_path="${CMSSW_BASE}/src/myMonoJetCode"       
+cfg_path="${base_path}/config"
+src_path="${base_path}/src"
+
+cd $src_path
+
+# add common options
+commonOptions=""
+
+# add specific options
+options_sig="$commonOptions "
+options_zmumu="$commonOptions "
+options_zee="$commonOptions "
+options_wmnu="$commonOptions "
+options_wenu="$commonOptions "
+options_gam="$commonOptions "
+
 echo
 echo
-./tmp/main $MY_CFG_PATH/monojet_signalRegion_config.txt
+./tmp/main ${cfg_path}/monojet_signalRegion_config.txt $options_sig
 echo
 echo
-./tmp/main $MY_CFG_PATH/zmumujets_ControlRegion_config.txt
+./tmp/main ${cfg_path}/zmumujets_ControlRegion_config.txt $options_zmumu
 echo
 echo
-./tmp/main $MY_CFG_PATH/wmunujets_ControlRegion_config.txt
+./tmp/main ${cfg_path}/wmunujets_ControlRegion_config.txt $options_wmunu
 echo
 echo
-./tmp/main $MY_CFG_PATH/zeejets_ControlRegion_config.txt
+./tmp/main ${cfg_path}/zeejets_ControlRegion_config.txt $options_zee
 echo
 echo
-./tmp/main $MY_CFG_PATH/wenujets_ControlRegion_config.txt
+./tmp/main ${cfg_path}/wenujets_ControlRegion_config.txt $options_wenu
 echo
 echo
-./tmp/main $MY_CFG_PATH/gammajets_ControlRegion_config.txt
+./tmp/main ${cfg_path}/gammajets_ControlRegion_config.txt $options_gam
 echo
 echo
-#echo "producing plots for monojet (exclusive)"
+echo "producing plots for monojet (exclusive)"
 ./../macro/distribution 0
 echo
 echo
-#echo "producing plots for monoV"
+echo "producing plots for monoV"
 ./../macro/distribution 1
 echo
 echo
